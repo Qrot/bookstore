31aa3d9c6ed70a098dc076a14b8ce64096521d38
justshmily <32678276+justshmily@users.noreply.github.com>
2018-08-23 13:58:19 +0800

    登录

---
+++ src/main/java/com/qrot/bookstore/background/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.qrot.bookstore.background.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qrot.bookstore.background.model.User;
+import com.qrot.bookstore.background.service.BackUserService;
+
+/**
+ * 控制器
+ * @author 15295
+ *
+ */
+@RestController
+@RequestMapping("/api/1")
+@CrossOrigin
+public class UserController {
+	
+	@Autowired
+	BackUserService service;
+
+	@GetMapping("/login")
+	@ResponseBody
+	public String login(
+			@RequestParam(name="n") String name,
+			@RequestParam(name="p") String password) {
+		String msg;
+		
+		if(!("admin".equals(name))) {
+			msg = "用户名错误！";
+			
+		}else {
+			User user = service.login(name);
+			if(user.getPassword().equals(password)) {
+				msg="OK";
+			}else {
+				msg="密码错误";
+			}
+		}
+		
+		return msg;
+	}
+}
---
+++ src/main/java/com/qrot/bookstore/background/model/User.java
@@ -0,0 +1,40 @@
+package com.qrot.bookstore.background.model;
+
+/**
+ *  用户实体类
+ * @author 15295
+ *
+ */
+public class User {
+
+	private int id;
+	private String name;
+	private String password;
+	
+	public void User() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
